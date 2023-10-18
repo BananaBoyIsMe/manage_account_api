211cf67bd96eb723c0a86b1fab8dec6bbcbc4c8c
BananaDog <145420584+BananaBoyIsMe@users.noreply.github.com>
2023-10-19 06:24:00 +0700

    Update file.routes.js

--- app/routes/file.routes.js
+++ app/routes/file.routes.js
@@ -2,6 +2,6 @@ module.exports = (app) => {
   const file_controller = require("../controllers/file.controller");
   var router = require("express").Router();
   router.post("/upload", file_controller.upload);
-  router.post("/:name", file_controller.displayAvatar);
+  router.get("/:name", file_controller.displayAvatar);
   app.use("/api/file", router);
 };
